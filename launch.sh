c39983474174307cd24683c673cc5979b6daf03c
Aniket Pandey <aniketp@FreeBSD.org>
2018-06-06 01:49:58 +0530

    Update the Cliff version to 2.3.0

--- launch.sh
+++ launch.sh
@@ -1,8 +1,9 @@
 cd conf; rm tomcat-users.xml; wget https://raw.githubusercontent.com/ahalterman/CLIFF-up/master/tomcat-users.xml
 $CATALINA_HOME/bin/startup.sh
 
-cd; wget https://github.com/c4fcm/CLIFF/releases/download/v2.1.1/CLIFF-2.1.1.war
-mv CLIFF-2.1.1.war /usr/local/tomcat/webapps/
+CLIFF_VERSION=2.3.0
+cd; wget https://github.com/c4fcm/CLIFF/releases/download/v${CLIFF_VERSION}/CLIFF-${CLIFF_VERSION}.war
+mv CLIFF-${CLIFF_VERSION}.war /usr/local/tomcat/webapps/
 
 apt-get install -y git openjdk-7-jdk openjdk-7-doc openjdk-7-jre-lib maven
 cd; git clone https://github.com/Berico-Technologies/CLAVIN.git
